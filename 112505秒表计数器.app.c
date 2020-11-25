a4f6506de2840589561eb1cfe24eb55b30e95774
1005zz <74358718+1005zz@users.noreply.github.com>
2020-11-25 21:47:57 +0800

    Create 112505秒表计数器.app.c

---
+++ 112505秒表计数器.app.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<windows.h>
+void main(){
+int x=1;
+printf("秒表计时器\n");
+do{
+printf("现在是好多秒\n",x);
+x++;
+Sleep(1000);
+}
+while(x<=1000000000000);{
+printf("303030303");
+}
+
+
+}
