64b4610c4d3bd2cfc390cf8f4109d1752a894d47
1005zz <74358718+1005zz@users.noreply.github.com>
2020-12-02 21:15:21 +0800

    Create 120203.秒转时分秒.app.c

--- 120203.秒转时分秒.app.c
+++ 120203.秒转时分秒.app.c
@@ -4,12 +4,12 @@ void main(){
 	printf("请输入秒数\n");
 	scanf("%d",&a);
 	b=a/3600;
-	c=a/60;
+	c=(a%3600)/60;
 	d=a-b*3600-c*60;
 	if(a>=0&&a<=24*3600-1){
 	printf("%02d:",b);
 	printf("%02d:",c);
-	printf("%02d:",d);
+	printf("%02d",d);
 	}
 	else{
 	printf("输入错误，请重新输入");
