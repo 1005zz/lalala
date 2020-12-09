dd8b459899a620ae12fe843c9b6cf361ca0b29c3
1005zz <74358718+1005zz@users.noreply.github.com>
2020-12-09 16:37:54 +0800

    Update and rename 120903.字符的输出.app.c to 120903.字符串的输出.app.c

---
+++ 120903.字符串的输出.app.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+void main(){
+	char array_2[]={"锦鲤护我，锦鲤护我，水逆走开，水逆走开"};
+	printf("%s\n",array_2);
+}
--- 120903.字符的输出.app.c
+++
@@ -1,5 +0,0 @@
-#include<stdio.h>
-void main(){
-	char array_2[]={"锦鲤护我，锦鲤护我"};
-	printf("%s\n",array_2);
-}
