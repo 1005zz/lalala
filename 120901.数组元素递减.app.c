12a5346776aa188cc016bb7182a8951aeae8ae7f
1005zz <74358718+1005zz@users.noreply.github.com>
2020-12-09 09:37:59 +0800

    Create 120901.数组元素递减.app.c

---
+++ 120901.数组元素递减.app.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<windows.h>
+void main(){
+int value=91;
+int b,c;
+int a[9][10]={
+	{0,0,0,0,0,0,0,0,0,0}
+};
+for(b=0;b<9;b++){
+	for(c=0;c<10;c++){
+		value--;
+		a[b][c]=value;
+		printf("%d ",a[b][c]);
+	}
+	printf("\n");
+}
+
+
+}
